184805f6e278ac40ab951805ef5f3358a08368cd
PravinElangovan <pravinkumarelangovan20@gmail.com>
2023-05-15 18:57:54 +0530

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Comp from "./Comp";
+
 
 const App = () => {
   return (
     <div id="main">
-    <p>I am learning React. My life is getting better.</p>
+      <Comp />
     </div>
   )
 }
